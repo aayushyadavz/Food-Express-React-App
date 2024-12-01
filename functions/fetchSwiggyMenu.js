1f929f8c2529683750e0d7b8ac3d1a93ee08e23f
Ayush Yadav <ayushyadav24nov1234@gmail.com>
2024-12-01 12:19:28 +0530

    updated fetch menu url

--- functions/fetchSwiggyMenu.js
+++ functions/fetchSwiggyMenu.js
@@ -1,10 +1,5 @@
 const fetch = require("node-fetch");
 
-export const MENU_URL =
-  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId=";
-
-export const REST_OF_MENU_URL = "&catalog_qa=undefined&submitAction=ENTER";
-
 exports.handler = async (event, context) => {
   const { resId } = event.queryStringParameters;
 
@@ -17,7 +12,9 @@ exports.handler = async (event, context) => {
 
   try {
     // Replace with your actual restaurant menu API
-    const response = await fetch(MENU_URL + resId + REST_OF_MENU_URL);
+    const response = await fetch(
+      `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`
+    );
     const data = await response.json();
 
     return {
