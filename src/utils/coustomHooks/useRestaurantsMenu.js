47b1f73a7f355dd4c60c07bf60b6213083bf1772
Ayush Yadav <ayushyadav24nov1234@gmail.com>
2024-12-01 13:07:31 +0530

    fixes

--- src/utils/coustomHooks/useRestaurantsMenu.js
+++ src/utils/coustomHooks/useRestaurantsMenu.js
@@ -11,7 +11,9 @@ const useRestaurantsMenu = (resId) => {
   }, [resId]);
 
   const fetchMenu = async () => {
-    const response = await fetch(`/.netlify/functions/fetchSwiggyMenu`); // Putting resId between the Api URL
+    const response = await fetch(
+      `/.netlify/functions/fetchSwiggyMenu?resId=${resId}`
+    ); // Putting resId between the Api URL
     const data = await response.json();
     setResMenu(data.data);
   };
